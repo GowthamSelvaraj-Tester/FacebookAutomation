348ff39cbde6cabdf713cbebcc3b240696d531ba
GowthamS <gowtham.s@perfutil.com>
2024-07-05 12:04:35 +0530

    commit for adding log-report-screenshot-listenerclasses

--- FacebookAutomation/src/main/java/base/Base.java
+++ FacebookAutomation/src/main/java/base/Base.java
@@ -1,75 +1,42 @@
 package base;
 
 import java.io.IOException;
+import java.util.Collections;
+
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import utility.ConfigReaders;
 import utility.GlobalVariable;
 
-public class Base 
-	{
-		public WebDriver driver;
+public class Base  {
+	public WebDriver driver;
 		
-		public WebDriver initializedriver(String appUrl) throws IOException
-			{
-				String browser = ConfigReaders.getProperty("browsers");
-			
-				if(browser.matches("Chrome")) 
-					{
-			    		System.getProperty(ConfigReaders.getProperty("Chromedriver"),GlobalVariable.basepath+ConfigReaders.getProperty("chromedriverlocation"));
-			    		ChromeOptions option = new ChromeOptions();
-			    		option.addArguments("--disable-infobars");
-			    		option.addArguments("--incognito");
-			    		driver = new ChromeDriver(option);
-			    		driver.manage().window().maximize();
-			    		driver.manage().deleteAllCookies();
-			    		driver.get(appUrl);
-					}
-				else if(browser.matches("Firefox"))
-					{
-						System.getProperty(ConfigReaders.getProperty("Firefoxdriver"),GlobalVariable.basepath+ConfigReaders.getProperty("firefoxdriverlocation"));
-						FirefoxOptions option = new FirefoxOptions();
-						option.addArguments("--disable-infobars");
-						option.addArguments("--incognito");
-						driver = new FirefoxDriver(option);
-						driver.manage().window().maximize();
-						driver.manage().deleteAllCookies();
-						driver.get(appUrl);
-					}
-				else if(browser.matches("Edge"))
-					{
-						System.getProperty(ConfigReaders.getProperty("Edgedriver"),GlobalVariable.basepath+ConfigReaders.getProperty("edgedriverlocation"));
-						EdgeOptions option = new EdgeOptions();
-						option.addArguments("--disable-infobars");
-						option.addArguments("--incognito");
-						driver = new EdgeDriver(option);
-						driver.manage().window().maximize();
-						driver.manage().deleteAllCookies();
-						driver.get(appUrl);
-					}
-				else
-					{
-						System.getProperty(ConfigReaders.getProperty("Chromedriver"),GlobalVariable.basepath+ConfigReaders.getProperty("chromedriverlocation"));
-						ChromeOptions option = new ChromeOptions();
-						option.addArguments("--disable-infobars");
-						option.addArguments("--incognito");
-						driver = new ChromeDriver(option);
-						driver.manage().window().maximize();
-						driver.manage().deleteAllCookies();
-						driver.get(appUrl);
-					}
-			
-				return driver;
-			}		
-		
-		public void closebrowser(WebDriver driver)
-			{
-				driver.quit();
-			}
-	
+	public WebDriver initializedriver(String URL) throws IOException {
+		// Set the system property for ChromeDriver location
+		System.getProperty(ConfigReaders.getProperty("chromeDriver"),GlobalVariable.basepath+ConfigReaders.getProperty("chromeDriverLocation"));
+		ChromeOptions option = new ChromeOptions(); // Create ChromeOptions to customize browser settings
+		option.addArguments("--disable-cache");// Disable cache
+		option.addArguments("--disable-cookies"); // Disable cookies
+		option.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation")); // Exclude automation switches to avoid detection as a bot
+		option.setExperimentalOption("useAutomationExtension", false); // Disable use of automation extension
+		option.setPageLoadStrategy(PageLoadStrategy.NORMAL);// Set page load strategy to normal
+//		option.addArguments("--headless");// Headless mode for running tests without GUI (commented out)
+		option.addArguments("--disable-infobars"); // Disable infobars
+		option.addArguments("--incognito"); // Launch browser in incognito mode
+		driver = new ChromeDriver(option); // Initialize ChromeDriver with options 
+		driver.manage().window().maximize();// Maximize browser window
+		driver.manage().deleteAllCookies();// Delete all cookies
+		driver.get(URL);// Navigate to the specified URL
+		return driver; // Return the WebDriver instance
 	}
+
+  public void closebrowser(WebDriver driver){
+		driver.quit();// Quit the browser session
+	}	
+
+  public WebDriver getDriver() {
+    	return driver;// Return the current WebDriver instance
+	}
+}
--- FacebookAutomation/src/main/java/utility/ResourceUtility.java
+++ FacebookAutomation/src/main/java/utility/ResourceUtility.java
@@ -1,9 +1,12 @@
 package utility;
 
-public class ResourceUtility 
-	{
-		public static String getFolderPath()
-		{
-			return GlobalVariable.basepath+ConfigReaders.getProperty("testDataPath");
-		}
+public class ResourceUtility  {
+
+	public static String getFolderPath(){
+		return GlobalVariable.basepath+ConfigReaders.getProperty("testDataPath");
 	}
+	
+	public static String getScreenshotPath() {
+		return GlobalVariable.basepath+ConfigReaders.getProperty("screenShotFolderPath");
+	}
+}
